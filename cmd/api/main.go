90507bc9eebb6cc4ba750ef8138525e9c527602a
Henrique Veronez <v3ronez.dev@gmail.com>
2025-03-15 15:37:27 -0300

    heathcheck method

---
+++ cmd/api/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func main() {
+	var config config
+
+	flag.IntVar(&config.port, "port", 8080, "API server port")
+	flag.StringVar(&config.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	app := &application{
+		config: config,
+		logger: logger}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", config.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+	logger.Info("Starting server", "addr", srv.Addr, "env", config.env)
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/v3ronez/yes-another-api-in-golang
 
 go 1.24.1
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
